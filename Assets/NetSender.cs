2a2404dcf1083b3c2b54a9fb783523ec9009febd
Warrick Corfe-Tan <warrickctjunk@gmail.com>
2018-02-12 15:40:19 +1300

    Created bare bone reliable channel data sending application

---
+++ Assets/NetSender.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetSender : MonoBehaviour {
+
+    public Text inputText;
+    public Text displayText;
+
+    GlobalConfig gConfig;
+
+    public int port = 12345;
+    public string connectionTargetIp = "127.0.0.1";
+    public int connectionLimit = 10;
+
+    int hostId;
+    int connectionId;
+    int myReliableChannelId;
+    int myUnreliableChannelId;
+
+    // Use this for initialization
+    void Start () {
+        StartHost();
+        ConnectToHost();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ReceiveData();
+        //Debug.Log("connection id: " + connectionId);
+	}
+
+    void StartHost()
+    {
+        // Init Transport using default values.
+        NetworkTransport.Init();
+
+        // Create a connection config and add a Channel.
+        ConnectionConfig config = new ConnectionConfig();
+        myReliableChannelId = config.AddChannel(QosType.Reliable);
+
+        // Create a topology based on the connection config.
+        HostTopology topology = new HostTopology(config, 10);
+
+        // Create a host based on the topology we just created, and bind the socket to port 12345.
+        hostId = NetworkTransport.AddHost(topology, port);
+    }
+
+    void ConnectToHost()
+    {
+        byte error;
+        connectionId = NetworkTransport.Connect(hostId, "127.0.0.1", port, 0, out error);
+    }
+
+    void SendData(byte[] buffer)
+    {
+        byte error;
+        int bufferLength = buffer.Length;
+        NetworkTransport.Send(hostId, connectionId, myReliableChannelId, buffer, bufferLength, out error);
+    }
+
+    void ReceiveData()
+    {
+        int outHostId;
+        int outConnectionId;
+        int outChannelId;
+        byte[] buffer = new byte[1024];
+        int bufferSize = 1024;
+        int receiveSize;
+        byte error;
+
+        NetworkEventType evnt = NetworkTransport.Receive(out outHostId, out outConnectionId, out outChannelId, buffer, bufferSize, out receiveSize, out error);
+        switch (evnt)
+        {
+            case NetworkEventType.ConnectEvent:
+                if (outHostId == hostId &&
+                    outConnectionId == connectionId &&
+                    (NetworkError)error == NetworkError.Ok)
+                {
+                    Debug.Log("Connected");
+                }
+                break;
+            case NetworkEventType.DataEvent:
+                Debug.Log("Received data from hostid: " + outHostId + " on connection id: " + outConnectionId);
+                DecodeData(buffer);
+                break;
+        }
+    }
+
+    void DecodeData(byte[] buffer)
+    {
+        string decodedData = System.Text.ASCIIEncoding.ASCII.GetString(buffer);
+        displayText.text = decodedData.ToUpper();
+        Debug.Log(decodedData.ToUpper());
+    }
+
+    void Disconnect()
+    {
+        byte error;
+        NetworkTransport.Disconnect(hostId, connectionId, out error);
+    }
+
+    public void SendText()
+    {
+        string text = inputText.text;
+        byte[] buffer = System.Text.Encoding.ASCII.GetBytes(text);
+        SendData(buffer);
+    }
+}
---
+++ Assets/NetTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetTest : MonoBehaviour {
+
+    int connectionId;
+    int channelId;
+    int hostId;
+
+    void Start()
+    {
+        // Init Transport using default values.
+        NetworkTransport.Init();
+
+        // Create a connection config and add a Channel.
+        ConnectionConfig config = new ConnectionConfig();
+        channelId = config.AddChannel(QosType.Reliable);
+
+        // Create a topology based on the connection config.
+        HostTopology topology = new HostTopology(config, 10);
+
+        // Create a host based on the topology we just created, and bind the socket to port 12345.
+        hostId = NetworkTransport.AddHost(topology, 12345);
+
+        // Connect to the host with IP 10.0.0.42 and port 54321
+        byte error;
+        connectionId = NetworkTransport.Connect(hostId, "127.0.0.1", 12345, 0, out error);
+    }
+
+    void Update()
+    {
+        int outHostId;
+        int outConnectionId;
+        int outChannelId;
+        byte[] buffer = new byte[1024];
+        int bufferSize = 1024;
+        int receiveSize;
+        byte error;
+
+        NetworkEventType evnt = NetworkTransport.Receive(out outHostId, out outConnectionId, out outChannelId, buffer, bufferSize, out receiveSize, out error);
+        switch (evnt)
+        {
+            case NetworkEventType.ConnectEvent:
+                if (outHostId == hostId &&
+                    outConnectionId == connectionId &&
+                    (NetworkError)error == NetworkError.Ok)
+                {
+                    Debug.Log("Connected");
+                }
+                break;
+            case NetworkEventType.DisconnectEvent:
+                if (outHostId == hostId &&
+                    outConnectionId == connectionId)
+                {
+                    Debug.Log("Connected, error:" + error.ToString());
+                }
+                break;
+        }
+    }
+}
